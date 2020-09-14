6ad76b3e261d440b4167a61fe6481430b91c3b16
Frank Aguinaga <frankaguinaga10@gmail.com>
2020-09-14 18:29:47 -0500

    upload Strings

---
+++ 04-Java-Spring/01-Spring-Fundamentals/01-Strings/Strings/src/test/java/com/franka/strings/StringsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franka.strings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
